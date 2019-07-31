cc9a9890128e4f2196cb2d21ec3704a2b024dd51
JTProg <mail@jtprog.ru>
2019-07-31 19:16:33 +0300

    Add JSON for formating message;

--- main.py
+++ main.py
@@ -11,49 +11,44 @@ __author_email__ = 'mail@jtprog.ru'
 from pyzabbix import ZabbixAPI
 import sys
 from utils import *
-import urllib3
-
-import logging
-
-# Disable SSL warning
-urllib3.disable_warnings()
-# Load environment
-
-# Logging configuration
-logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d]# '
-                           u'%(levelname)s [%(asctime)s]  %(message)s',
-                    level=logging.DEBUG,
-                    filename=env['LOG_FILE_PATH'])
 
 #  Zabbix API
 zapi = ZabbixAPI(env['ZBX_SERVER'])
 zapi.session.verify = False
 # 1.1 Login to Zabbix
 zapi.login(user=env['ZBX_USER'], password=env['ZBX_PASS'])
-logging.debug("Connected to Zabbix API Version %s" % zapi.api_version())
+# logging.info("Connected to Zabbix API Version %s" % zapi.api_version())
 
 
-try:
-    # Get event object with {EVENT.ID}
-    event = zapi.event.get(eventids=sys.argv[1])[0]
-    # Check Acknowledge field
-    if event['acknowledged'] is '0':
-        keyid = create_issue(title=sys.argv[2],
-                             body=sys.argv[3],
-                             project=env['JIRA_PROJECT'],
-                             issuetype=env['JIRA_ISSUE_TYPE'],
-                             priority=env['JIRA_ISSUE_PRIORITY'])
+def main():
+    try:
+        # Get event object with {EVENT.ID}
+        amsg = parse_message(sys.argv[1])
+        event = zapi.event.get(eventids=amsg['event_id'])[0]
+        # Check Acknowledge field
+        if event['acknowledged'] is '0':
+            body = create_message(amsg)
+            keyid = create_issue(title=amsg['subject'],
+                                 body=body,
+                                 project=env['JIRA_PROJECT'],
+                                 issuetype=env['JIRA_ISSUE_TYPE'],
+                                 priority=env['JIRA_ISSUE_PRIORITY'])
+            zapi.event.acknowledge(eventids=int(amsg['event_id']), action=6, message=keyid)
+        elif event['acknowledged'] is '1':
+            issue_key = zapi.event.get(eventids=int(amsg['event_id']),
+                                       output='extend',
+                                       select_acknowledges='extend')[0]
+            issue_key = issue_key['acknowledges'][0]['message']
+            # Classification issue
+            close_issue(issue=issue_key, status=env['JIRA_TRANSITION_CLASSIF'])
+            # Close issue
+            close_issue(issue=issue_key, status=env['JIRA_TRANSITION_CLOSE'])
 
-        zapi.event.acknowledge(eventids=int(event['eventid']), action=6, message=keyid)
-    elif event['acknowledged'] is '1':
-        issue_key = zapi.event.get(eventids=sys.argv[1], output='extend', select_acknowledges='extend')[0]
-        issue_key = issue_key['acknowledges'][0]['message']
-        close_issue(issue=issue_key, status=env['JIRA_TRANSITION_CLASSIF'])
-        close_issue(issue=issue_key, status=env['JIRA_TRANSITION_CLOSE'])
+
+    except Exception as e:
+        logging.critical("[*] CRITICAL: %s" % e)
 
 
-except Exception as e:
-    logging.debug("[*] EXCEPTION: %s" % e)
-
-
-
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
--- utils.py
+++ utils.py
@@ -9,31 +9,30 @@ __version__ = '0.0.1'
 __author_email__ = 'mail@jtprog.ru'
 
 from jira import JIRA
-import dotenv as d
-from pathlib import Path
-
-env = d.get_variables(str(Path(__file__).parent / '.env'))
+from config import *
+import json
 
 
 # Login to Jira API
 def jira_login():
     """
-    Login to Jira server
+    jira_login()
     :return: JIRA object
     """
     jira_server = {'server': env['JIRA_SERVER']}
     return JIRA(options=jira_server, basic_auth=(env['JIRA_USER'], env['JIRA_PASS']))
 
 
-def create_issue(title: str, body: str, project: str, issuetype: str, priority: str) -> str:
+# Create issue
+def create_issue(title, body, project, issuetype, priority) -> str:
     """
-    Creating issue in Jira
+    create_issue()
     :param title:
     :param body:
     :param project:
     :param issuetype:
     :param priority:
-    :return:
+    :return: KeyID -> str
     """
     jira = jira_login()
     issue_params = {
@@ -46,6 +45,7 @@ def create_issue(title: str, body: str, project: str, issuetype: str, priority: 
     return jira.create_issue(fields=issue_params).key
 
 
+# Close issue
 def close_issue(issue, status):
     """
     close_issue()
@@ -56,4 +56,37 @@ def close_issue(issue, status):
     jira = jira_login()
     jira.transition_issue(issue, status)
 
+
+# Parse message from {ALERT.MESSAGE}
+def parse_message(msg):
+    """
+    Load JSON data from Zabbix {ALERT.MESSAGE}
+    :param msg: JSON as string
+    :return: JSON obj
+    """
+    res = json.loads(str(msg))
+    return res
+
+
+# Create message for Jira
+def create_message(pre_msg):
+    """
+    Create pre-format message for body in Jira issue
+    :param pre_msg: JSON-object
+    :return: string
+    """
+    msg = 'Host: {host_name} \n\
+Trigger: {trigger_name} \n\
+Trigger status: {trigger_status} \n\
+Trigger severity: {trigger_severity} \n\
+Trigger URL: {problem_url} \n\
+Item values: {item_value} \n\
+Original event ID: {event_id} '.format(host_name=pre_msg['host_name'],
+                                       trigger_name=pre_msg['trigger_name'],
+                                       trigger_status=pre_msg['trigger_status'],
+                                       trigger_severity=pre_msg['trigger_severity'],
+                                       problem_url=pre_msg['problem_url'],
+                                       item_value=pre_msg['item_value'],
+                                       event_id=pre_msg['event_id'])
+    return msg
 
